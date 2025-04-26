56aa3e56ae320c2c9da4a7e36953066466330786
Fuguety <luczevedo@gmail.com>
2025-04-26 20:15:53 +0200

    Added search bar

--- app/src/main/java/app/main/upsaplayer/HomePage.java
+++ app/src/main/java/app/main/upsaplayer/HomePage.java
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import androidx.appcompat.widget.SearchView;
+
+
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,4 +40,54 @@ public class HomePage extends AppCompatActivity
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setQueryHint("Search songs...");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener()
+        {
+            @Override
+            public boolean onQueryTextSubmit(String query)
+            {
+                // When user presses search button
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText)
+            {
+                List<Music> filteredList = new ArrayList<>();
+
+                for (Music music : musicList)
+                {
+                    if (music.getTitle().toLowerCase().contains(newText.toLowerCase()))
+                    {
+                        filteredList.add(music);
+                    }
+                }
+
+                adapter = new MusicAdapter(filteredList);
+                recyclerView.setAdapter(adapter);
+
+                return true;
+            }
+
+        });
+
+        return true;
+    }
+
+
+
+
+
+
 }
--- app/src/main/java/app/main/upsaplayer/MusicAdapter.java
+++ app/src/main/java/app/main/upsaplayer/MusicAdapter.java
@@ -1,34 +1,42 @@
 package app.main.upsaplayer;
 
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.List;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder>
 {
     private List<Music> musicList;
+    private List<Music> fullMusicList;
 
     public static class ViewHolder extends RecyclerView.ViewHolder
     {
         public TextView musicTitle;
+        public ImageView menuButton;
 
         public ViewHolder(View view)
         {
             super(view);
             musicTitle = view.findViewById(R.id.musicTitle);
+            menuButton = view.findViewById(R.id.menu_button);
         }
     }
 
+
+
     public MusicAdapter(List<Music> musicList)
     {
-        this.musicList = musicList;
+        this.musicList = new ArrayList<>(musicList);
+        this.fullMusicList = new ArrayList<>(musicList);
     }
 
     @Override
@@ -44,7 +52,50 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.ViewHolder>
     {
         Music music = musicList.get(position);
         holder.musicTitle.setText(music.getTitle());
+
+        holder.menuButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), holder.menuButton);
+                MenuInflater inflater = popupMenu.getMenuInflater();
+                inflater.inflate(R.menu.music_item_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener()
+                {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item)
+                    {
+                        int id = item.getItemId();
+
+                        if (id == R.id.rename)
+                        {
+                            // Handle rename
+                            return true;
+                        }
+                        else if (id == R.id.add_to_playlist)
+                        {
+                            // Handle add to playlist
+                            return true;
+                        }
+                        else if (id == R.id.delete)
+                        {
+                            // Handle delete
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                });
+
+                popupMenu.show();
+            }
+        });
     }
+
 
     @Override
     public int getItemCount()
